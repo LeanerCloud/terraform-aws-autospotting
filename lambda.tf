bb506d0984a448131aadd407646b72bf09f92ba1
Brian Findlay <bfindlay@fastmail.com>
2019-02-08 15:12:29 -0500

    Use label module to generate names and tags for terraform resources

--- lambda.tf
+++ lambda.tf
@@ -1,11 +1,11 @@
 module "aws_lambda_function" {
   source = "./lambda"
 
-  lambda_zipname = "${var.lambda_zipname}"
+  label_context = "${module.label.context}"
 
-  lambda_s3_bucket = "${var.lambda_s3_bucket}"
-  lambda_s3_key    = "${var.lambda_s3_key}"
-
+  lambda_zipname     = "${var.lambda_zipname}"
+  lambda_s3_bucket   = "${var.lambda_s3_bucket}"
+  lambda_s3_key      = "${var.lambda_s3_key}"
   lambda_role_arn    = "${aws_iam_role.autospotting_role.arn}"
   lambda_runtime     = "${var.lambda_runtime}"
   lambda_timeout     = "${var.lambda_timeout}"
@@ -27,14 +27,14 @@ module "aws_lambda_function" {
 }
 
 resource "aws_iam_role" "autospotting_role" {
-  name                  = "autospotting"
+  name                  = "${module.label.id}"
   path                  = "/lambda/"
   assume_role_policy    = "${file("${path.module}/lambda-policy.json")}"
   force_detach_policies = true
 }
 
 resource "aws_iam_role_policy" "autospotting_policy" {
-  name   = "policy_for_autospotting"
+  name   = "policy_for_${module.label.id}"
   role   = "${aws_iam_role.autospotting_role.id}"
   policy = "${file("${path.module}/autospotting-policy.json")}"
 }
@@ -54,11 +54,11 @@ resource "aws_cloudwatch_event_target" "cloudwatch_target" {
 }
 
 resource "aws_cloudwatch_event_rule" "cloudwatch_frequency" {
-  name                = "autospotting_frequency"
+  name                = "${module.label.id}_frequency"
   schedule_expression = "${var.lambda_run_frequency}"
 }
 
 resource "aws_cloudwatch_log_group" "log_group_autospotting" {
-  name              = "/aws/lambda/${module.aws_lambda_function.function_name}"
+  name              = "/aws/lambda/${module.label.id}"
   retention_in_days = 7
 }
