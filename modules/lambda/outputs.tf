e4a07df77338574ec37a9756db6c0c79fae1c69a
bleloch <leo@blelo.ch>
2021-03-12 12:49:43 +0000

    Make outputs compatible with count

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,7 +1,7 @@
 output "function_name" {
-  value = aws_lambda_function.autospotting.function_name
+  value = aws_lambda_function.autospotting[*].function_name
 }
 
 output "arn" {
-  value = aws_lambda_function.autospotting.arn
+  value = aws_lambda_function.autospotting[*].arn
 }
