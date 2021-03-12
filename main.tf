44ae002f293561911a436fe4f1b23c15400a5c2e
Leo Brookes <leo.brookes@cognitoiq.com>
2021-03-12 16:33:58 +0000

    Refactored

--- main.tf
+++ main.tf
@@ -43,36 +43,6 @@ resource "aws_lambda_function" "autospotting" {
   }
 }
 
-module "aws_lambda_function" {
-  source = "./modules/lambda"
-
-  label_context = module.label.context
-
-  lambda_zipname     = var.lambda_zipname
-  lambda_s3_bucket   = var.lambda_s3_bucket
-  lambda_s3_key      = var.lambda_s3_key
-  lambda_role_arn    = join("", aws_iam_role.autospotting_role[*].arn)
-  lambda_runtime     = var.lambda_runtime
-  lambda_timeout     = var.lambda_timeout
-  lambda_memory_size = var.lambda_memory_size
-  lambda_tags        = var.lambda_tags
-
-  autospotting_enabled                      = var.autospotting_enabled
-  autospotting_allowed_instance_types       = var.autospotting_allowed_instance_types
-  autospotting_disallowed_instance_types    = var.autospotting_disallowed_instance_types
-  autospotting_instance_termination_method  = var.autospotting_instance_termination_method
-  autospotting_min_on_demand_number         = var.autospotting_min_on_demand_number
-  autospotting_min_on_demand_percentage     = var.autospotting_min_on_demand_percentage
-  autospotting_on_demand_price_multiplier   = var.autospotting_on_demand_price_multiplier
-  autospotting_spot_price_buffer_percentage = var.autospotting_spot_price_buffer_percentage
-  autospotting_spot_product_description     = var.autospotting_spot_product_description
-  autospotting_bidding_policy               = var.autospotting_bidding_policy
-  autospotting_regions_enabled              = var.autospotting_regions_enabled
-  autospotting_tag_filters                  = var.autospotting_tag_filters
-  autospotting_tag_filtering_mode           = var.autospotting_tag_filtering_mode
-  autospotting_license                      = var.autospotting_license
-}
-
 resource "aws_iam_role" "autospotting_role" {
   count                 = var.autospotting_enabled ? 1 : 0
   name                  = module.label.id
@@ -118,4 +88,3 @@ resource "aws_cloudwatch_log_group" "log_group_autospotting" {
   name              = "/aws/lambda/${module.label.id}"
   retention_in_days = 7
 }
-
--- modules/lambda/main.tf
+++
@@ -1,5 +0,0 @@
-module "label" {
-  source  = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.13.0"
-  context = var.label_context
-}
-
